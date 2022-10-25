79fa9cb24c4e9732f9e97861bde48af60d1df581
Adrian Ramos <airria2000@gmail.com>
2022-10-25 00:32:15 -0500

    Fixed project...

---
+++ app/src/main/java/com/visionapi/clotho/CategorySelector.java
@@ -0,0 +1,14 @@
+package com.visionapi.clotho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategorySelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_selector);
+    }
+}
