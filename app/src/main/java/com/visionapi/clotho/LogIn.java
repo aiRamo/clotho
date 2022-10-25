5105fa3e18d411bf6a7d250a15ed8dffc9558502
naila269 <inaila019@gmail.com>
2022-10-25 02:07:19 -0500

    Admin

--- app/src/main/java/com/visionapi/clotho/LogIn.java
+++ app/src/main/java/com/visionapi/clotho/LogIn.java
@@ -18,7 +18,14 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+import org.w3c.dom.Text;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -33,6 +40,23 @@ public class LogIn extends AppCompatActivity{
         setContentView(R.layout.activity_signin);
 
         //TODO: When the login button is clicked, check if both text fields contain 'admin', if they do, redirect the app to Homepage
+
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText password = (EditText) findViewById(R.id.Password);
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().
+                        equals("admin")){
+                }else{
+                    Toast.makeText(LogIn.this,"Login Failed",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
 
 
         signUpButton = findViewById(R.id.signup);
