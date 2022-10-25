cecc3013ea81b13e971076bcc4177bf3eabe37f4
Adrian Ramos <airria2000@gmail.com>
2022-10-25 00:06:49 -0500

    Fixed project...

--- app/src/main/java/com/visionapi/clotho/MainActivity.java
+++ app/src/main/java/com/visionapi/clotho/MainActivity.java
@@ -1,14 +1,44 @@
 package com.visionapi.clotho;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button newEntryButton = findViewById(R.id.btn_newEntry);
+        Intent imagePreview = new Intent(MainActivity.this, PreviewHandler.class);
+
+        newEntryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(imagePreview);
+            }
+        });
+
+
     }
+
 }
