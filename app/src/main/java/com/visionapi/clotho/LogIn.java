40b0bbdd8a50414981951e7a47d4c6ae2443be1f
Adrian Ramos <airria2000@gmail.com>
2022-11-29 22:11:10 -0600

    Revamped product search page, added skeleton for amazon redirect

--- app/src/main/java/com/visionapi/clotho/LogIn.java
+++ app/src/main/java/com/visionapi/clotho/LogIn.java
@@ -1,31 +1,13 @@
 package com.visionapi.clotho;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.Manifest;
-import android.app.Activity;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.button.MaterialButton;
-
-import org.w3c.dom.Text;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -76,6 +58,7 @@ public class LogIn extends AppCompatActivity{
         });
 
     // Need RegisterButton code but keeps showing error
+
 
     }
 }
--- app/src/main/java/com/visionapi/clotho/Registration.java
+++ app/src/main/java/com/visionapi/clotho/Registration.java
@@ -1,11 +1,10 @@
 package com.visionapi.clotho;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Registration extends AppCompatActivity {
 
