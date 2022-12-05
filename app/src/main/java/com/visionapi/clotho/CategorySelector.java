d075e51af3ac7a5d312a3cf0692636d69f31603d
Adrian Ramos <airria2000@gmail.com>
2022-12-05 13:02:40 -0600

    Revamped product search page, added skeleton for amazon redirect

--- app/src/main/java/com/visionapi/clotho/CategorySelector.java
+++ app/src/main/java/com/visionapi/clotho/CategorySelector.java
@@ -3,7 +3,6 @@ package com.visionapi.clotho;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
@@ -23,7 +22,6 @@ public class CategorySelector extends AppCompatActivity {
 
 
         TakePhoto = findViewById(R.id.btn_takePhoto);
-        Intent imagePreview = new Intent(CategorySelector.this, PreviewHandler.class);
         Intent kotlinPreview = new Intent(CategorySelector.this, ProductSearchAPIClient.class);
 
 
