37302eeaa3d7dabc08f3b5904e539770c21b855b
Grant Codes It <grantzelenay@yahoo.com>
2022-12-07 12:03:11 -0600

    almost

--- app/src/main/java/com/visionapi/clotho/CategorySelector.java
+++ app/src/main/java/com/visionapi/clotho/CategorySelector.java
@@ -4,16 +4,19 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class CategorySelector extends AppCompatActivity {
+public class CategorySelector extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     public static final int PERMISSION_CODE = 1000;
     Button TakePhoto;
@@ -41,16 +44,33 @@ public class CategorySelector extends AppCompatActivity {
         });
         //Checks to see if pants button is clicked and will than show the take photo button
         Button b = (Button) findViewById(R.id.btn_takePhoto);
-        ToggleButton pantsbutton = (ToggleButton) findViewById(R.id.Pants);
+        Spinner pantsbutton = (Spinner) findViewById(R.id.colorSpinner);
         pantsbutton.setOnClickListener( new View.OnClickListener()  {
             public void onClick(View v) {
                 b.setVisibility(View.VISIBLE);
-                System.out.println("Pants Selector Clicked");
+                System.out.println("Color Selector Clicked");
             }
         });
+        /// Color Drop down
+        String[] colorList = {"1", "2", "3", "4", "5","6","7","8","9","10","11","12","13","14"};
+        setContentView(R.layout.activity_category_selector);
+        Spinner spin = (Spinner) findViewById(R.id.colorSpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, colorList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+        spin.setOnItemSelectedListener(this);
 
 
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
 
--- app/src/main/java/com/visionapi/clotho/Settings.java
+++ app/src/main/java/com/visionapi/clotho/Settings.java
@@ -6,34 +6,33 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Settings extends AppCompatActivity {
+public class Settings extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     //Shoe Variables
+    String[] users = {"1", "2", "3", "4", "5","6","7","8","9","10","11","12","13","14"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
-
         //New shoe drop down
-        final Spinner dropdowns = findViewById(R.id.dropdown);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(
-                this,
-                R.array.spinner_items,
-                R.layout.color_spinner_layout
-        );
-        adapter.setDropDownViewResource(R.layout.spinner_dropdown_layout);
-        dropdowns.setAdapter(adapter);
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Spinner spin = (Spinner) findViewById(R.id.dropdown);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, users);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+        spin.setOnItemSelectedListener(this);
 
-        ImageButton settingsButton = (ImageButton)findViewById(R.id.imageButton);
-        settingsButton.setOnClickListener( new View.OnClickListener()  {
+
+        ImageButton settingsButton = (ImageButton) findViewById(R.id.imageButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(Settings.this, MainActivity.class));
                 System.out.println("Settings Button Clicked");
@@ -42,5 +41,13 @@ public class Settings extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(getApplicationContext(), "Shoe Size: "+ users[adapterView.getSelectedItemPosition()] ,Toast.LENGTH_SHORT).show();
+    }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
