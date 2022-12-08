37b002001eab7cc720913839fb4849e684045ef0
Grant Codes It <grantzelenay@yahoo.com>
2022-12-07 18:15:28 -0600

    yadasdad

--- app/src/main/java/com/visionapi/clotho/CategorySelector.java
+++ app/src/main/java/com/visionapi/clotho/CategorySelector.java
@@ -14,6 +14,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.visionapi.clotho.databinding.ActivityMainBinding;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 
 public class CategorySelector extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
@@ -25,10 +26,12 @@ public class CategorySelector extends AppCompatActivity implements AdapterView.O
     ColorObject selectedColor;
     ActivityMainBinding binding;
 
+     Spinner pantsbutton = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        pantsbutton = (Spinner) findViewById(R.id.colorSpinner);
         setContentView(R.layout.activity_category_selector);
 
         TakePhoto = findViewById(R.id.btn_takePhoto);
@@ -47,6 +50,7 @@ public class CategorySelector extends AppCompatActivity implements AdapterView.O
         });
         //Checks to see if pants button is clicked and will than show the take photo button
         Button b = (Button) findViewById(R.id.btn_takePhoto);
+        b.setVisibility(View.VISIBLE);
         /*Spinner pantsbutton = (Spinner) findViewById(R.id.colorSpinner);
         pantsbutton.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -56,18 +60,15 @@ public class CategorySelector extends AppCompatActivity implements AdapterView.O
             }
         });*/
 
-        Spinner pantsbutton = (Spinner) findViewById(R.id.colorSpinner);
-
         ArrayAdapter adapter = ArrayAdapter.createFromResource(
                 this,
-                R.array.shoe_sizes,
+                R.array.colorList,
                 R.layout.color_spinner_layout
         );
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_layout);
         pantsbutton.setAdapter(adapter);
-        pantsbutton.setOnItemSelectedListener(this);{
-            b.setVisibility(View.VISIBLE);
-        }
+        pantsbutton.setOnItemSelectedListener(this);
+
 
         /// Color Drop down
         /*String[] colorList = {"1", "2", "3", "4", "5","6","7","8","9","10","11","12","13","14"};
@@ -83,8 +84,14 @@ public class CategorySelector extends AppCompatActivity implements AdapterView.O
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String colorText = pantsbutton.getSelectedItem().toString();
+        Toast.makeText(this, colorText, Toast.LENGTH_SHORT).show();
         ConstraintLayout root=(ConstraintLayout) findViewById(R.id.categoryStuff);
-        root.setBackgroundColor(Color.parseColor("#000000"));
+        if (colorText.equals("Blue")) {
+            root.setBackgroundColor(Color.parseColor("@colors/Blue"));
+            Toast.makeText(this, "Blue Shit", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     @Override
