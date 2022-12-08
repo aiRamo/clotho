158619a368d969b4a1e479a77433d7a85203f272
Adrian Ramos <airria2000@gmail.com>
2022-12-07 23:56:36 -0600

    Added Image saving :)

--- app/src/main/java/com/visionapi/clotho/MainActivity.java
+++ app/src/main/java/com/visionapi/clotho/MainActivity.java
@@ -2,12 +2,17 @@ package com.visionapi.clotho;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +25,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Intent loginPage = new Intent(MainActivity.this, LogIn.class);
+
+        final ImageButton button1 = (ImageButton) findViewById(R.id.outfit1);
+        final ImageButton button2 = (ImageButton) findViewById(R.id.outfit2);
+        final ImageButton button3 = (ImageButton) findViewById(R.id.outfit3);
+        final ImageButton button4 = (ImageButton) findViewById(R.id.outfit4);
+        final ImageButton button5 = (ImageButton) findViewById(R.id.outfit5);
+
+        ArrayList<DataSnapshot> savedData = new ArrayList<>();
+        ArrayList<ImageButton> imageButtonList = new ArrayList<>();
+
+        imageButtonList.add(button1);
+        imageButtonList.add(button2);
+        imageButtonList.add(button3);
+        imageButtonList.add(button4);
+        imageButtonList.add(button5);
+
+        savedData = GlobalVars.savedData;
+
+        Uri uri;
+
+
+        if (GlobalVars.savedCount != 0){
+
+            for (int i = 0; i < GlobalVars.savedCount; i++){
+                uri = Uri.parse((String) savedData.get(i).child("imageuri").getValue());
+                imageButtonList.get(i).setImageURI(uri);
+            }
+
+        }
+
 
 
         if (!GlobalVars.isLoggedIn){
@@ -45,6 +80,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         categoryButton.setBackgroundColor(Color.BLUE);
+
+
     }
 
 }
